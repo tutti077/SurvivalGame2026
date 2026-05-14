3c06c94ffb340912b388e22215bbf7064fed110c
Tutti <mark.tuttavilla@gmail.com>
2026-05-14 00:30:51 -0400

    delete old crap

--- Code/GameBuildLabel.cs
+++ Code/GameBuildLabel.cs
@@ -5,5 +5,5 @@ namespace Game;
 /// </summary>
 public static class GameBuildLabel
 {
-	public const string Display = "0.3.88";
+	public const string Display = "0.3.92";
 }
--- Code/Weapons/WeaponType.cs
+++
@@ -1,8 +0,0 @@
-namespace Game;
-
-/// <summary>High-level weapon classification for items, UI, and attack routing.</summary>
-public enum WeaponType
-{
-	None,
-	Sword
-}
